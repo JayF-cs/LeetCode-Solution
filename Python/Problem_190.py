74ec0f178419e106c0b1e32d870a23bf5613a1e2
James Fryar <jfryar07@gmail.com>
2026-02-16 14:39:33 -0500

    More readable solution to 190

--- Python/Problem_190.py
+++ Python/Problem_190.py
@@ -5,3 +5,13 @@ class Solution:
         n &= 0xFFFFFFFF
         #return the reversed binary converted back to int
         return (int(f"{n:032b}"[::-1],2))
+    
+    def reverseBitsBetter(self, n: int) -> int:
+
+        result = 0
+        #Loop in range in 32 to ensure 32 bits
+        for i in range(32):
+            result = (result << 1) | (n & 1)
+            n >> 1
+        
+        return result
